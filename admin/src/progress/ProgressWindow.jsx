727026b1ee8632d1ab40e054465ac7d4a3f0785a
yusufhacktiv8 <yusuf.hacktiv8@gmail.com>
2018-10-09 23:40:46 +0700

    Add Project Progress fields

--- admin/src/progress/ProgressWindow.jsx
+++ admin/src/progress/ProgressWindow.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Modal, Form, InputNumber, Button, Row, Col, message } from 'antd';
+import { Modal, Form, InputNumber, Button, Row, Col, Tabs, message } from 'antd';
 import axios from 'axios';
 import showError from '../utils/ShowError';
 import YearSelect from '../common/YearSelect';
@@ -9,6 +9,7 @@ import ProjectSelect from '../project/ProjectSelect';
 const PROGRESSES_URL = `${process.env.REACT_APP_SERVER_URL}/api/projectprogresses`;
 
 const FormItem = Form.Item;
+const TabPane = Tabs.TabPane;
 
 class ProgressWindow extends Component {
   state = {
@@ -50,6 +51,7 @@ class ProgressWindow extends Component {
     const { getFieldDecorator } = form;
     return (
       <Modal
+        wrapClassName="vertical-center-modal"
         visible={visible}
         title="Project Progress"
         okText="Save"
@@ -101,26 +103,132 @@ class ProgressWindow extends Component {
               </FormItem>
             </Col>
           </Row>
-          <Row gutter={10}>
-            <Col span={12}>
-              <FormItem label="Ra Progress">
-                {getFieldDecorator('raProgress', {
-                  initialValue: progress.raProgress,
-                })(
-                  <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
-                )}
-              </FormItem>
-            </Col>
-            <Col span={12}>
-              <FormItem label="Ri Progress">
-                {getFieldDecorator('riProgress', {
-                  initialValue: progress.riProgress,
-                })(
-                  <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
-                )}
-              </FormItem>
-            </Col>
-          </Row>
+          <Tabs defaultActiveKey="1" type="card">
+            <TabPane forceRender tab="Tab 1" key="1">
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Ra Progress">
+                    {getFieldDecorator('raProgress', {
+                      initialValue: progress.raProgress,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Ri Progress">
+                    {getFieldDecorator('riProgress', {
+                      initialValue: progress.riProgress,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Ra">
+                    {getFieldDecorator('ra', {
+                      initialValue: progress.ra,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Ri">
+                    {getFieldDecorator('ri', {
+                      initialValue: progress.ri,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="PDP">
+                    {getFieldDecorator('pdp', {
+                      initialValue: progress.pdp,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="BAD">
+                    {getFieldDecorator('bad', {
+                      initialValue: progress.bad,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane forceRender tab="Tab 2" key="2">
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="OK">
+                    {getFieldDecorator('ok', {
+                      initialValue: progress.ok,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Cashflow">
+                    {getFieldDecorator('cashflow', {
+                      initialValue: progress.cashflow,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Piutang Usaha">
+                    {getFieldDecorator('piutangUsaha', {
+                      initialValue: progress.piutangUsaha,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Piutang Retensi">
+                    {getFieldDecorator('piutangRetensi', {
+                      initialValue: progress.piutangRetensi,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+              <Row gutter={10}>
+                <Col span={12}>
+                  <FormItem label="Tagihan Bruto">
+                    {getFieldDecorator('tagihanBruto', {
+                      initialValue: progress.tagihanBruto,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+                <Col span={12}>
+                  <FormItem label="Persediaan">
+                    {getFieldDecorator('persediaan', {
+                      initialValue: progress.persediaan,
+                    })(
+                      <InputNumber min={-1000000000} max={1000000000} step={0.1} precision={2} style={{ width: '100%' }} />,
+                    )}
+                  </FormItem>
+                </Col>
+              </Row>
+            </TabPane>
+          </Tabs>
         </Form>
       </Modal>
     );
