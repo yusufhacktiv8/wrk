fe2adc189f6c6809fcda7e8cd48c082e24265f2c
Yusuf <myusuf.arifien@btpnsyariah.com>
2019-08-31 07:52:06 +0700

    Home score card

--- api/controllers/home.js
+++ api/controllers/home.js
@@ -13,6 +13,7 @@ const CREDITS_KEY = 'CREDITS';
 const NET_PROFITS_KEY = 'NET_PROFITS';
 const EPC_SUMS_KEY = 'EPC_SUMS';
 const OM_SUMS_KEY = 'OM_SUMS';
+const SCORE_SUMS_KEY = 'SCORE_SUMS';
 
 const DIVIDER = 1000;
 
@@ -204,6 +205,80 @@ const findOmSum = (year, month) => (
   })
 );
 
+const findScoreSum = (year, month) => (
+  new Promise((resolve, reject) => {
+    models.Score.findAll({
+      where: {
+        year: year || MINIMUM_YEAR,
+      },
+      attributes: [
+        'month', 
+        'scoreType', 
+        [sequelize.fn('sum', sequelize.col('raScore')), 'raScoreSum'],
+        [sequelize.fn('sum', sequelize.col('riScore')), 'riScoreSum'],
+      ],
+      group : ['month', 'scoreType'],
+      raw: true,
+      order: ['month', 'scoreType']
+    })
+    .then((scoreSums) => {
+      let flattenScoreSums = [];
+      for(let i = 0; i < scoreSums.length; i += 1) {
+        let scoreSum = scoreSums[i];
+        const scoreSumMonth = scoreSum.month;
+        const scoreSumType = scoreSum.scoreType;
+        const raScoreSum = scoreSum.raScoreSum;
+        const riScoreSum = scoreSum.riScoreSum;
+        let flattenScoreSum = flattenScoreSums.find(obj => obj.month === scoreSumMonth);
+        
+        if (!flattenScoreSum) {
+          flattenScoreSum = {
+            month: scoreSumMonth,
+            qmslRaScoreSum: 0,
+            qmslRiScoreSum: 0,
+            sheRaScoreSum: 0,
+            sheRiScoreSum: 0,
+            r5RaScoreSum: 0,
+            r5RiScoreSum: 0
+          }
+          flattenScoreSums.push(flattenScoreSum);
+        }
+        switch(scoreSumType) {
+          case 1:
+            flattenScoreSum.qmslRaScoreSum = raScoreSum;
+            flattenScoreSum.qmslRiScoreSum = riScoreSum;
+            break;
+          case 2:
+            flattenScoreSum.sheRaScoreSum = raScoreSum;
+            flattenScoreSum.sheRiScoreSum = riScoreSum;
+            break;
+          case 3:
+            flattenScoreSum.r5RaScoreSum = raScoreSum;
+            flattenScoreSum.r5RiScoreSum = riScoreSum;
+            break;
+        }
+      }
+      
+      resolve({
+        key: SCORE_SUMS_KEY,
+        result: flattenScoreSums.map(obj => {
+          return {
+            month: obj.month,
+            qmslRaScoreSum: obj.qmslRaScoreSum / DIVIDER,
+            qmslRiScoreSum: obj.qmslRiScoreSum / DIVIDER,
+            sheRaScoreSum: obj.sheRaScoreSum / DIVIDER,
+            sheRiScoreSum: obj.sheRiScoreSum / DIVIDER,
+            r5RaScoreSum: obj.r5RaScoreSum / DIVIDER,
+            r5RiScoreSum: obj.r5RiScoreSum / DIVIDER,
+          }
+        }),
+      });
+    }).catch((err) => {
+      reject(err);
+    });
+  })
+);
+
 exports.findByMonthYear = function findByMonthYear(req, res) {
     const { month, year } = req.query;
     let promises = [];
@@ -213,6 +288,7 @@ exports.findByMonthYear = function findByMonthYear(req, res) {
     promises.push(findNetProfits(year));
     promises.push(findEpcSum(year));
     promises.push(findOmSum(year));
+    promises.push(findScoreSum(year));
     Promise.all(promises)
     .then((results) => {
         const homeData = {};
@@ -222,12 +298,14 @@ exports.findByMonthYear = function findByMonthYear(req, res) {
         const netProfits = results.find(o => o.key === NET_PROFITS_KEY);
         const epcSums = results.find(o => o.key === EPC_SUMS_KEY);
         const omSums = results.find(o => o.key === OM_SUMS_KEY);
+        const scoreSums = results.find(o => o.key === SCORE_SUMS_KEY);
         homeData['omzets'] = omzets.result;
         homeData['sales'] = sales.result;
         homeData['credits'] = credits.result;
         homeData['netProfits'] = netProfits.result;
         homeData['epcSums'] = epcSums.result;
         homeData['omSums'] = omSums.result;
+        homeData['scoreSums'] = scoreSums.result;
         
         res.json(homeData);
     })
