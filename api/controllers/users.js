96c91629379aa9e9af38271549ac8dc762500a0b
Yusuf <myusuf.arifien@btpnsyariah.com>
2021-05-12 23:39:48 +0700

    Fix users

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,65 +1,66 @@
-const bcrypt = require('bcrypt');
-const models = require('../models');
-const { getUserId } = require('../helpers/AuthUtils');
+const bcrypt = require("bcrypt");
+const models = require("../models");
+const { getUserId } = require("../helpers/AuthUtils");
+const { Op } = require("sequelize");
 
 const saltRounds = 10;
 
 const sendError = (err, res) => {
-  res.status(500).send(`Error while doing operation: ${err.name}, ${err.message}`);
+  res
+    .status(500)
+    .send(`Error while doing operation: ${err.name}, ${err.message}`);
 };
 
 exports.findAll = function findAll(req, res) {
-  const searchText = req.query.searchText ? `%${req.query.searchText}%` : '%%';
+  const searchText = req.query.searchText ? `%${req.query.searchText}%` : "%%";
   const limit = req.query.pageSize ? parseInt(req.query.pageSize, 10) : 10;
-  const currentPage = req.query.currentPage ? parseInt(req.query.currentPage, 10) : 1;
+  const currentPage = req.query.currentPage
+    ? parseInt(req.query.currentPage, 10)
+    : 1;
   const offset = (currentPage - 1) * limit;
   models.User.findAndCountAll({
     where: {
-      $or: [
-        { username: { $ilike: searchText } },
-        { name: { $ilike: searchText } },
+      [Op.or]: [
+        { username: { [Op.iLike]: searchText } },
+        { name: { [Op.iLike]: searchText } },
       ],
     },
-    include: [
-      { model: models.Role },
-    ],
+    include: [{ model: models.Role }],
     limit,
     offset,
   })
-  .then((result) => {
-    res.json(result);
-  })
-  .catch((err) => {
-    sendError(err, res);
-  });
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      sendError(err, res);
+    });
 };
 
 exports.usersByRole = function findAll(req, res) {
   const roleCode = req.query.role;
   models.User.findAll({
     where: {},
-    include: [
-      { model: models.Role, where: { code: roleCode } },
-    ],
+    include: [{ model: models.Role, where: { code: roleCode } }],
   })
-  .then((result) => {
-    res.json(result);
-  })
-  .catch((err) => {
-    sendError(err, res);
-  });
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      sendError(err, res);
+    });
 };
 
 exports.findOne = function findOne(req, res) {
   models.User.findOne({
     where: { id: req.params.userId },
   })
-  .then((user) => {
-    res.json(user);
-  })
-  .catch((err) => {
-    sendError(err, res);
-  });
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      sendError(err, res);
+    });
 };
 
 exports.create = function create(req, res) {
@@ -68,8 +69,7 @@ exports.create = function create(req, res) {
 
   models.Role.findOne({
     where: { id: roleId },
-  })
-  .then((role) => {
+  }).then((role) => {
     bcrypt.hash(userForm.password, saltRounds, (err, hash) => {
       models.User.create({
         username: userForm.username,
@@ -77,15 +77,14 @@ exports.create = function create(req, res) {
         password: hash,
         email: userForm.email,
       })
-      .then((user) => {
-        user.setRole(role)
-        .then((result) => {
-          res.json(result);
+        .then((user) => {
+          user.setRole(role).then((result) => {
+            res.json(result);
+          });
+        })
+        .catch((errCreate) => {
+          sendError(errCreate, res);
         });
-      })
-      .catch((errCreate) => {
-        sendError(errCreate, res);
-      });
     });
   });
 };
@@ -97,26 +96,23 @@ exports.update = function update(req, res) {
   models.User.findOne({
     where: { id: req.params.userId },
   })
-  .then((user) => {
-    models.Role.findOne({
-      where: { id: roleId },
-    })
-    .then((role) => {
-      user.setRole(role)
-      .then((updateResult) => {
-        user.name = userForm.name;
-        user.email = userForm.email;
+    .then((user) => {
+      models.Role.findOne({
+        where: { id: roleId },
+      }).then((role) => {
+        user.setRole(role).then((updateResult) => {
+          user.name = userForm.name;
+          user.email = userForm.email;
 
-        user.save()
-        .then((saveResult) => {
-          res.json(saveResult);
+          user.save().then((saveResult) => {
+            res.json(saveResult);
+          });
         });
       });
+    })
+    .catch((err) => {
+      sendError(err, res);
     });
-  })
-  .catch((err) => {
-    sendError(err, res);
-  });
 };
 
 exports.changePassword = function changePassword(req, res) {
@@ -125,73 +121,70 @@ exports.changePassword = function changePassword(req, res) {
   models.User.findOne({
     where: { id: req.params.userId },
   })
-  .then((user) => {
-    // bcrypt.compare(form.currentPassword, user.password, (err, valid) => {
-    //   if (valid) {
-    //     bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
-    //       user.password = hash;
-    //       user.save()
-    //       .then((saveResult) => {
-    //         res.json(saveResult);
-    //       });
-    //     });
-    //   } else {
-    //     res.send('Wrong password', 400);
-    //   }
-    // });
-    bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
-      user.password = hash;
-      user.save()
-      .then((saveResult) => {
-        res.json(saveResult);
-      });
-    });
-  })
-  .catch((err) => {
-    sendError(err, res);
-  });
-};
-
-exports.changeMyPassword = function changeMyPassword(req, res) {
-  const form = req.body;
-  getUserId(req)
-  .then((userId) => {
-    models.User.findOne({
-      where: { id: userId },
-    })
     .then((user) => {
-      bcrypt.compare(form.currentPassword, user.password, (err, valid) => {
-        if (valid) {
-          bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
-            user.password = hash;
-            user.save()
-            .then((saveResult) => {
-              res.json(saveResult);
-            });
-          });
-        } else {
-          res.send('Wrong password', 403);
-        }
+      // bcrypt.compare(form.currentPassword, user.password, (err, valid) => {
+      //   if (valid) {
+      //     bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
+      //       user.password = hash;
+      //       user.save()
+      //       .then((saveResult) => {
+      //         res.json(saveResult);
+      //       });
+      //     });
+      //   } else {
+      //     res.send('Wrong password', 400);
+      //   }
+      // });
+      bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
+        user.password = hash;
+        user.save().then((saveResult) => {
+          res.json(saveResult);
+        });
       });
     })
     .catch((err) => {
       sendError(err, res);
     });
-  })
-  .catch(() => {
-    res.send('Unauthorized', 403);
-  });
+};
+
+exports.changeMyPassword = function changeMyPassword(req, res) {
+  const form = req.body;
+  getUserId(req)
+    .then((userId) => {
+      models.User.findOne({
+        where: { id: userId },
+      })
+        .then((user) => {
+          bcrypt.compare(form.currentPassword, user.password, (err, valid) => {
+            if (valid) {
+              bcrypt.hash(form.newPassword, saltRounds, (err2, hash) => {
+                user.password = hash;
+                user.save().then((saveResult) => {
+                  res.json(saveResult);
+                });
+              });
+            } else {
+              res.send("Wrong password", 403);
+            }
+          });
+        })
+        .catch((err) => {
+          sendError(err, res);
+        });
+    })
+    .catch(() => {
+      res.send("Unauthorized", 403);
+    });
 };
 
 exports.destroy = function destroy(req, res) {
-  models.User.destroy(
-    {
-      where: { id: req.params.userId },
-    })
-  .then((result) => {
-    res.json(result);
+  models.User.destroy({
+    where: { id: req.params.userId },
   })
-  .catch((err) => {
-    sendError(err, res);
-  });
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      sendError(err, res);
+    });
 };
